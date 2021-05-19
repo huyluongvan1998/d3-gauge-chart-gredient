9b77d4ffae884c0041ba0fa4a923470fb8ced6bd
Huy Luong <huy.luong@successsoftware.global>
2021-05-19 17:51:27 +0700

    update chart

--- src/app/gauge-chart/gauge-chart.component.ts
+++ src/app/gauge-chart/gauge-chart.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
 import * as d3 from 'd3';
-import { text } from 'd3';
 
 export interface IThat {
   update: any,
@@ -17,291 +16,274 @@ export class GaugeChartComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.Gauge({
-      size: 300
-    }, 500);
+    this.Gauge(450);
+
   }
 
 
   //@function config
-  Gauge = function (configuration, value) {
-    let config = {
-      size: 300,
-      arcInset: 200,
-      arcWidth: 90,
-
-      pointerWidth: 1,
-      pointerOffset: 0,
-      pointerHeadLengthPercent: 0.9,
-
-      minValue: 300,
-      maxValue: 850,
-
-      minAngle: -90,
-      maxAngle: 90,
-
-      transitionMs: 750,
-
-      //@label style
-      //curent label Style
-      currentLabelFontSize: 48,
-      currentLabelInset: 20,
-      //min and max label
-      labelFont: "Helvetica",
-      labelFontSize: 20,
-      labelTextFontSize: 12,
-      //color for each milestone
-      arcColorFn: function (value) {
-        let ticks = [
-          {
-            tick: 299,
-            color: 'green',
-          },
-          {
-            tick: 350,
-            color: 'yellow',
-          },
-          {
-            tick: 450,
-            color: 'orange',
-          },
-          {
-            tick: 550,
-            color: 'red',
-          },
-        ];
-        //Change the color of gauge bar when reach specific value
-        let ret;
-        ticks.forEach(tick => {
-          if (value > tick.tick) {
-            ret = tick.color;
-            return;
-          }
-        });
-        return ret;
-      },
-    };
-    //@function clone config from configuration method
-    function configure(configuration) {
-      for (let prop in configuration) {
-        config[prop] = configuration[prop];
-      }
-    }
-    configure(configuration);
-
-    //Variables for styling and sizing gauge chart
-    let foreground, arc, svg, current;
-    let cur_color;
-    let new_color, hold;
-
-    var oR = config.size - config.arcInset;
-    var iR = config.size - oR - config.arcWidth;
-
-    //Convert to radian to apply for render gauge chart
-    function deg2rad(deg) {
-      return (deg * Math.PI) / 180;
-    }
-
-    //@BIG function
-    //render the gauge chart
-    function render() {
-      // oR = 150;
-      // iR = 50;
-
-      // Arc Defaults
-      let arcData = [
-        { startAngle: deg2rad(-90), endAngle: deg2rad(-48) },
-        { startAngle: deg2rad(-45), endAngle: deg2rad(0) },
-        { startAngle: deg2rad(2), endAngle: deg2rad(45) },
-        { startAngle: deg2rad(47), endAngle: deg2rad(90) }
-      ];
-      let arcSeparator = [
-        { startAngle: deg2rad(-48), endAngle: deg2rad(-46) },
-        { startAngle: deg2rad(0), endAngle: deg2rad(2) },
-        { startAngle: deg2rad(45), endAngle: deg2rad(47) },
-      ];
-
-
-      let d1arc = d3.arc().innerRadius(iR).outerRadius(oR).startAngle(deg2rad(-90)).endAngle(deg2rad(-47));
-      let d2arc = d3.arc().innerRadius(iR).outerRadius(oR).startAngle(deg2rad(-45)).endAngle(deg2rad(-0));
-      let d3arc = d3.arc().innerRadius(iR).outerRadius(oR).startAngle(deg2rad(2)).endAngle(deg2rad(45));
-      let d4arc = d3.arc().innerRadius(iR).outerRadius(oR).startAngle(deg2rad(47)).endAngle(deg2rad(90));
-
-      arc = d3.arc()
-        .innerRadius(iR)
-        .outerRadius(oR).startAngle(deg2rad(-90)).endAngle(deg2rad(-40));
-
-
-      // Place svg element
-      svg = d3.select("body").append("svg")
-        .attr("width", config.size)
-        .attr("height", config.size)
-        .append("g")
-        .attr("transform", "translate(" + config.size / 2 + "," + config.size / 2 + ")");
-
-      //CREATE GRADIENT COLOR 
-      const createGradient = select => {
-        const gradient = select
-          .select('defs')
-          .append('linearGradient')
-          .attr('id', 'gradient');
-        gradient
-          .append('stop')
-          .attr('offset', '0%')
-          .attr('style', 'stop-color:green;stop-opacity:1');
-
-        gradient
-          .append('stop')
-          .attr('offset', '50%')
-          .attr('style', 'stop-color:yellow;stop-opacity:1');
-        gradient
-          .append('stop')
-          .attr('offset', '100%')
-          .attr('style', 'stop-color:red;');
+  Gauge = function (value) {
+    const { PI, cos, sin, round } = Math;
+    let margin = { top: 10, bottom: 10, left: 10, right: 10 };
+    let width = 300 + margin.left + margin.right;
+    let height = 300 + margin.top + margin.bottom;
+    function polarToCartesian(
+      cx: number,
+      cy: number,
+      radius: number,
+      angleInDegrees: number,
+    ) {
+      var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+      return {
+        x: cx + radius * Math.cos(angleInRadians),
+        y: cy + radius * Math.sin(angleInRadians),
       };
+    }
+
+    function circlePath(x: number,
+      y: number,
+      radius: number,
+      startAngle: number,
+      endAngle: number,) {
+      var start = polarToCartesian(x, y, radius, endAngle * 0.9999);
+      var end = polarToCartesian(x, y, radius, startAngle);
+      var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+      const d = [
+        'M',
+        start.x,
+        start.y,
+        'A',
+        radius,
+        radius,
+        0,
+        largeArcFlag,
+        0,
+        end.x,
+        end.y,
+
+      ];
+      return {
+        start,
+        end,
+        d: d.join(' '),
+      };
+    }
+    const size = width - 32;
+    const strokeWidth = 11;
+    const r = (size - strokeWidth) / 2;
+    const cx = size / 2;
+    const cy = size / 2;
+    const A = 50;
+
+    function deg2rad(value) {
+      return value * PI / 180;
+    }
 
 
-      svg.append('defs');
-      svg.call(createGradient);
 
-      // Append background arc to svg
-      const d1Arc = svg
-        .append("path")
-        .attr("class", "d1-arc")
-        .style('fill', "#d8d8d8")
-        .attr("d", d1arc);
+    const d1Arc = circlePath(cx, cy, r, -90, -46);
+    const d2Arc = circlePath(cx, cy, r, -45, -1);
+    const d3Arc = circlePath(cx, cy, r, 0, 44);
+    const d4Arc = circlePath(cx, cy, r, 45, 89);
+    const dArc = d3.arc().innerRadius(cx + r).outerRadius(cx + r + strokeWidth).startAngle(deg2rad(-90));
 
-      var d2Arc = svg
-        .append("path")
-        .attr("id", "d2")
-        .style('fill', "#d8d8d8")
-        .attr("d", d2arc);
+    let progressPath;
 
-      var d3Arc = svg
-        .append("path")
-        .attr("id", "d3")
-        .style('fill', "#d8d8d8")
-        .attr("d", d3arc);
+    let ratio, color;
+    const part1Ratio = 41 / 180;
+    const part2Ratio = 91 / 180;
+    const part3Ratio = 141 / 180;
 
-      var d4Arc = svg
-        .append("path")
-        .attr("id", "d4")
-        .style('fill', "#d8d8d8")
-        .attr("d", d4arc);
+    function SemiCircularProgress(
+      progressValue,
+      totalValue,
+      startValue,
+      endValue
+    ) {
+      ratio = (progressValue - startValue) / (endValue - startValue);
+      console.log('Ratio: ', ratio);
+      console.log('Ratio 2: ', round(ratio * 180));
+      console.log('Part1: ', part1Ratio);
 
-      var animatedPath = svg
-        .append("path")
-        .attr("id", "progressPath")
-        .style('fill', 'red')
-        .attr("d", arc);
 
-      function polarToCartesian(cx: number, cy: number, radius: number, angleInDegrees: number) {
-        var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
-        return {
-          x: cx + radius * Math.cos(angleInRadians),
-          y: cy + radius * Math.sin(angleInRadians),
-        };
+
+      if (ratio > part3Ratio) {
+        progressPath = circlePath(cx, cy, r, 45, -90 + round(ratio * 180));
+        color = 'url(#linearGradient-4)';
+      } else if (ratio > part2Ratio) {
+        progressPath = circlePath(cx, cy, r, 0, -90 + round(ratio * 180));
+        color = 'url(#linearGradient-3)';
+      } else if (ratio > part1Ratio) {
+        progressPath = circlePath(cx, cy, r, -44.8, -90 + round(ratio * 180));
+        color = 'url(#linearGradient-2)';
+      } else {
+        progressPath = circlePath(cx, cy, r, -90, -90 + round(ratio * 180));
+        color = 'url(#linearGradient-1)';
       }
-      var cx, cy = (oR - iR) / 2;
-      var start = polarToCartesian(cx, cy, iR, -40 * 0.9999);
-      var end = polarToCartesian(cx, cy, iR, -90);
+    }
+    //@execute function for coordinate of processPath
+    SemiCircularProgress(value, 850, 300, 850);
 
-      var circle = svg
-        .append("circle")
-        .attr('cx', start.x)
-        .attr('cy', start.y)
+    function draw() {
+
+      const svg = d3.select('#gauge-chart').append('svg');
+      svg.attr('height', height)
+        .attr('width', width);
+      //@Gradient Linear.
+      //@Gradient 1
+      let defs = svg.append('defs');
+
+      //@Gradient 1
+      let linearG_1 = defs
+        .append('linearGradient')
+        .attr('id', 'linearGradient-1')
+        .attr('x1', 7.95)
+        .attr('y1', 0.264)
+        .attr('x2', -0.85)
+        .attr('y2', 1.099);
+      linearG_1.append('stop')
+        .attr('offset', 0)
+        .attr('stop-color', '#6dbf47');
+      linearG_1.append('stop')
+        .attr('offset', 0.399)
+        .attr('stop-color', '#8bd768');
+      linearG_1.append('stop')
+        .attr('offset', 0.727)
+        .attr('stop-color', '#dfc729');
+      linearG_1.append('stop')
+        .attr('offset', 1)
+        .attr('stop-color', '#e1281e');
+
+      //@Gradient 2
+      let linearG_2 = defs
+        .append('linearGradient')
+        .attr('id', 'linearGradient-2')
+        .attr('x1', 7.95)
+        .attr('y1', 0.264)
+        .attr('x2', -2.0)
+        .attr('y2', 3.075);
+      linearG_2.append('stop')
+        .attr('offset', 0)
+        .attr('stop-color', '#6dbf47');
+      linearG_2.append('stop')
+        .attr('offset', 0.35)
+        .attr('stop-color', '#8bd768');
+      linearG_2.append('stop')
+        .attr('offset', 0.828)
+        .attr('stop-color', '#dfc729');
+      linearG_2.append('stop')
+        .attr('offset', 1)
+        .attr('stop-color', '#e1281e');
+
+      //@Gradient 3
+      let linearG_3 = defs
+        .append('linearGradient')
+        .attr('id', 'linearGradient-3')
+        .attr('x1', 7.95)
+        .attr('y1', 0.264)
+        .attr('x2', -0.35)
+        .attr('y2', 1.099);
+      linearG_3.append('stop')
+        .attr('offset', 0)
+        .attr('stop-color', '#6dbf47');
+      linearG_3.append('stop')
+        .attr('offset', 0.3)
+        .attr('stop-color', '#8bd768');
+      linearG_3.append('stop')
+        .attr('offset', 0.828)
+        .attr('stop-color', '#6dbf47');
+      linearG_3.append('stop')
+        .attr('offset', 1)
+        .attr('stop-color', '#dfc729');
+
+      //@Gradient 4
+      console.log('ratio ', ratio === 1);
+      let linearG_4 = defs
+        .append('linearGradient')
+        .attr('id', 'linearGradient-4')
+        .attr('x1', 7.95)
+        .attr('y1', 0.264)
+        .attr('x2', -0.445)
+        .attr('y2', 1.099);
+      linearG_4.append('stop')
+        .attr('offset', 0)
+        .attr('stop-color', '#6dbf47');
+      linearG_4.append('stop')
+        .attr('offset', 0.86)
+        .attr('stop-color', '#6dbf47');
+      linearG_4.append('stop')
+        .attr('offset', 0.945)
+        .attr('stop-color', '#6dbf47');
+      linearG_4.append('stop')
+        .attr('offset', 0.978)
+        .attr('stop-color', '#6dbf47');
+      linearG_4.append('stop')
+        .attr('offset', 1)
+        .attr('stop-color', '#e1281e');
+
+
+
+      //@Gauge-chart
+      const g = svg.append('g')
+        .attr('transform', `translate(10, 10)`);
+
+      g
+        .attr('id', 'color-chart')
+        .attr('rotation', 90)
+        .attr('originX', width / 2)
+        .attr('originY', width / 2);
+
+      g.append('path')
+        .attr('d', d1Arc.d)
+        .attr('fill', 'none')
+        .attr('data-name', 'Path 1527')
+        .attr('stroke-width', strokeWidth)
+        .attr('stroke', `${ratio > part1Ratio ? 'url(#linearGradient-1)' : '#d8d8d8'}`);
+
+      g.append('path')
+        .attr('d', d2Arc.d)
+        .attr('fill', 'none')
+        .attr('data-name', 'Path 1527')
+        .attr('stroke-width', strokeWidth)
+        .attr('stroke', `${ratio > part2Ratio ? 'url(#linearGradient-2)' : '#d8d8d8'}`);
+      g.append('path')
+        .attr('d', d3Arc.d)
+        .attr('fill', 'none')
+        .attr('data-name', 'Path 1527')
+        .attr('stroke-width', strokeWidth)
+        .attr('stroke', `${ratio > part3Ratio ? 'url(#linearGradient-3)' : '#d8d8d8'}`);
+      g.append('path')
+        .attr('d', d4Arc.d)
+        .attr('fill', 'none')
+        .attr('data-name', 'Path 1527')
+        .attr('stroke-width', strokeWidth)
+        .attr('stroke', `${ratio === 1 ? 'url(#linearGradient-4)' : '#d8d8d8'}`);
+
+      g.append('path')
+        .attr('d', progressPath.d)
+        .attr('class', 'progress-path')
+        .attr('fill', 'none')
+        .attr('stroke-width', strokeWidth)
+        .attr('stroke', color);
+      g.append('circle')
+        .attr('cx', progressPath.start.x)
+        .attr('cy', progressPath.start.y)
         .attr('r', 8)
-        .attr('stroke', 'green')
-        .attr('stroke-width', 3)
-        .attr('fill', 'white');
-      // Display Max value
-      var max = svg
-        .append('text')
-        .attr('transform', 'translate(' + (iR + (oR - iR) / 2) + ',20)') // Set between inner and outer Radius
-        .attr('text-anchor', 'middle')
-        .style('font-family', config.labelFont)
-        .text(config.maxValue);
+        .attr('class', 'mini-circle')
+        .attr('stroke-width', 2)
+        .attr('stroke', '#8bd768')
+        .attr('fill', '#fff');
 
-      // Display Min value
-      var min = svg
-        .append('text')
-        .attr('transform', 'translate(' + -(iR + (oR - iR) / 2) + ',20)') // Set between inner and outer Radius
-        .attr('text-anchor', 'middle')
-        .style('font-size', config.labelFontSize)
-        .style('font-family', config.labelFont)
-        .text(config.minValue);
+      g.append('path')
+        .datum({ endAngle: deg2rad(-45) })
+        .attr('d', dArc)
+        .attr('class', 'testArc')
+        .each(function (d: any) {
+          var centerPoint = dArc.centroid(d);
+          console.log('center ', centerPoint);
+        });
 
-      // Display Current value
-      current = svg
-        .append('text')
-        .attr(
-          'transform',
-          'translate(0,' + -(-config.currentLabelInset + iR / 3) + ')'
-        ) // Push up from center 1/4 of innerRadius
-        .attr('text-anchor', 'middle')
-        .style('font-size', config.currentLabelFontSize)
-        .style('font-family', config.labelFont)
-        .style('letter-spacing', "0.48px")
-        .text(150); // insert current value to this
-
-      var LabelText = svg
-        .append('text')
-        .attr(
-          'transform',
-          'translate(0,' + -(-config.currentLabelInset + iR / 100) + ')'
-        )
-        .attr('text-anchor', 'middle')
-        .style('font-size', config.labelTextFontSize)
-        .style('font-weight', "bold")
-        .style('letter-spacing', "0.24px")
-        .style('fill', "#6cbe45")
-        .text('VERY GOOD');
     }
-    function update(value) {
-      // Get new color
-      new_color = config.arcColorFn(value);
-
-      var numPi = deg2rad(Math.floor(((value - 300) * 180) / (config.maxValue) - 90));
-
-      // Display Current value
-      current.transition().text(value);
-
-      // .text(config.labelFormat(value))
-
-
-
-      // Arc Transition
-      foreground
-        .transition()
-        .duration(config.transitionMs)
-        .style('fill', function () {
-          return 'url(#gradient)';
-        })
-        .call(arcTween, numPi);
-
-      // Set colors for next transition
-      hold = cur_color;
-      cur_color = new_color;
-      new_color = hold;
-    }
-
-    // Update animation
-    function arcTween(transition, newAngle) {
-      transition.attrTween('d', function (d) {
-        var interpolate = d3.interpolate(d.endAngle, newAngle);
-        return function (t) {
-          d.endAngle = interpolate(t);
-          return arc(d);
-        };
-      });
-    }
-
-    render();
-    // update(300);
-    // setInterval(() => {
-    //   update(400);
-    // }, 1500);
-
-    return config;
+    draw();
   };
-};
+}
